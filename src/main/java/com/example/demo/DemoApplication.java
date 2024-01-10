eacfa985b075d411b008ab84808b9a4da9990199
sandippatel13 <53583554+sandippatel13@users.noreply.github.com>
2024-01-09 20:44:29 -0500

    Add files via upload

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String getMessage() {
+		return "Hello Get API successful";
+	}
+	
+	@PostMapping("/{name}")
+	public String postMessage (@PathVariable String name) {
+		return "Your name is " + name;
+	}
+}
